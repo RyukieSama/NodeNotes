696e85a0ebcb5c446629fdd6edf234cb1541e3df
RyukieSama <ryukie.sama@gmail.com>
2018-02-28 14:32:47 +0800

    refactor

--- NodeStudy01/app.js
+++ NodeStudy01/app.js
@@ -16,42 +16,14 @@ const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 var fs = require('fs');
 
+const controller = require('./controller');
+
 const app = new Koa();
 
-function  addControllers(router) {
-    var files = fs.readdirSync(__dirname + '/controller');
-    var js_files = files.filter((f) => {
-            return f.endsWith('.js');
-        });
 
-    for (var f of js_files) {
-        console.log(`process controller: ${f}...`);
-        let mapping = require(__dirname + '/controller/' + f);
-        addMapping(router,mapping);
-    }
-}
 
-function addMapping(router,mapping) {
-    for (var url in mapping) {
-        if (url.startsWith('GET')) {
-
-            var path = url.substring(4);
-            router.get(path,mapping[url]);
-            console.log(`register URL mapping: GET ${path}`);
-
-        } else if (url.startsWith('POST ')) {
-
-            var path = url.substring(5);
-            router.post(path, mapping[url]);
-            console.log(`register URL mapping: POST ${path}`);
-
-        } else {
-            console.log(`invalid URL: ${url}`);
-        }
-    }
-}
-
-addControllers(router);
+// addControllers(router);
+app.use(controller());
 
 // add router middleware:
 // 由于middleware的顺序很重要，这个koa-bodyparser必须在router之前被注册到app对象上。
